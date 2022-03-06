eef93731f27a16ad79713d767340c26ea26c1490
Amine <amenaymane0@gmail.com>
2022-03-06 21:45:26 +0100

    fix some issues

--- JavaScript/hadith.js
+++ JavaScript/hadith.js
@@ -2,18 +2,24 @@ fetch("https://api.hadith.sutanlab.id/books/bukhari?range=1-300")
   .then((Response) => Response.json())
   .then((data) => {
     let hadihContent = document.getElementById("hadith-content-in-arabic");
+    let number = document.querySelector(".index");
     let i = 0;
     let j = 1;
     hadihContent.textContent = `${data.data.hadiths[i].arab}`;
+    number.textContent = `${i+1}/300`
+    hadihContent.textContent = `${data.data.hadiths[i].arab}`;
     let next = document.getElementById("next");
     let previous = document.getElementById("previous");
-    let number = document.querySelector(".index");
     next.addEventListener("click", () => {
       hadihContent.textContent = `${data.data.hadiths[(i += 1)].arab}`;
       number.textContent = `${(j += 1)}/300`;
     });
     previous.addEventListener("click", () => {
-      hadihContent.textContent = `${data.data.hadiths[(i -= 1)].arab}`;
+      if (j>1){
+        hadihContent.textContent = `${data.data.hadiths[(i -= 1)].arab}`;
         number.textContent = `${(j -= 1)}/300`
+      } else {
+        return
+      }
     });
   });
